f15c964336a6a7b4ae5322e91104af41358cbb55
shiluming <luming_bfm@163.com>
2014-08-14 22:59:22 +0800

    init src

---
+++ src/Thread/chap04/TraditionalThreadCommunication.java
@@ -0,0 +1,13 @@
+package Thread.chap04;
+
+public class TraditionalThreadCommunication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
