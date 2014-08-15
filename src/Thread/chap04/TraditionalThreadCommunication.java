43171173534ce0bde1f31cfbc3aa5b9ea5580583
shiluming <luming_bfm@163.com>
2014-08-15 16:56:00 +0800

    example chap04

--- src/Thread/chap04/TraditionalThreadCommunication.java
+++ src/Thread/chap04/TraditionalThreadCommunication.java
@@ -2,12 +2,77 @@ package Thread.chap04;
 
 public class TraditionalThreadCommunication {
 
-	/**
+	/**子线程循环10次，接着主线程循环100次，接着又回到子线程循环10次，接着再回到主线程循环100，如此循环50次
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		final Business business = new Business();
+		new Thread(
+				new Runnable(){
+					@Override
+					public void run() {
+						for(int i=1;i<=50;i++){
+							/*synchronized(TraditionalThreadCommunication.class){
+								
+								for(int j=1;j<=10;j++){
+									System.out.println("sub thread sequece of :"+ j + ",loop of "+i);
+								}							
+							}*/
+							business.sub(i);
+						}
+					}
+				}
+				
+		).start();
+		
+		for(int i=1;i<=50;i++){
+			/*synchronized(TraditionalThreadCommunication.class){
+				
+				for(int j=1;j<=10;j++){
+					System.out.println("main thread sequece of :"+ j + ",loop of "+i);
+				}							
+			}*/
+			business.main(i);
+		}		
 	}
-
+}
+/**
+ * 实现业务逻辑的类，把相关的资源，线程结合在一起，使代码更容易维护
+ * 主线程直接调用即可
+ * @author USER
+ *
+ */
+class Business{
+	private boolean bShouldSub = true;
+	public synchronized void sub(int i){
+		while(!bShouldSub){			//while比if好，能够多检查一遍，防止伪唤醒
+		//if(!bShouldSub){
+			try {
+				this.wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		for(int j=1;j<=10;j++){
+			System.out.println("sub thread sequece of :"+ j + ",loop of "+i);
+		}		
+		bShouldSub = false;
+		this.notify();//唤醒线程
+	}
+	public synchronized void main(int i){
+		if(bShouldSub){
+			try {
+				this.wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		for(int j=1;j<=100;j++){
+			System.out.println("main thread sequece of :"+ j + ",loop of "+i);
+		}		
+		bShouldSub = true;
+		this.notify();
+	}
 }
